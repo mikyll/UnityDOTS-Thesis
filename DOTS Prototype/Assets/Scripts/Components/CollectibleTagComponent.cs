ac40e385353570501318b86dfffa78a8d171f989
Michele Righi <56556806+mikyll@users.noreply.github.com>
2021-04-08 13:22:41 +0200

    Update CollectibleTagComponent.cs

--- DOTS Prototype/Assets/Scripts/Components/CollectibleTagComponent.cs
+++ DOTS Prototype/Assets/Scripts/Components/CollectibleTagComponent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 
-// questo componente viene assegnato dallo spawner a tutte le entities che possono essere raccolte dal/dai Player
 [GenerateAuthoringComponent]
 public struct CollectibleTagComponent : IComponentData
 {
